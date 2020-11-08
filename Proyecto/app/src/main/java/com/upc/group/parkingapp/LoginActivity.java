b084b62308a1cbeceafb66eb571732deabaf3f6c
hectorhans <hector.romerosandon@gmail.com>
2020-11-08 00:55:24 -0500

    commit inicial

---
+++ Proyecto/app/src/main/java/com/upc/group/parkingapp/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.upc.group.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        asignarReferencias();
+    }
+
+    private void asignarReferencias() {
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainMenuUsuario.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Proyecto/app/src/main/java/com/upc/group/parkingapp/MainMenuUsuario.java
@@ -0,0 +1,14 @@
+package com.upc.group.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenuUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_usuario);
+    }
+}
