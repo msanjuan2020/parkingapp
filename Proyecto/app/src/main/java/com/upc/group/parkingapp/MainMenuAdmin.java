967b9900f6598dc273b8bb3e4502b2062165417c
hectorhans <hector.romerosandon@gmail.com>
2020-11-08 13:22:12 -0500

    menu admin

---
+++ Proyecto/app/src/main/java/com/upc/group/parkingapp/MainMenuAdmin.java
@@ -0,0 +1,14 @@
+package com.upc.group.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainMenuAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_admin);
+    }
+}
