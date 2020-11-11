2320067272fe86f3d8655d8e262c026e337ec38b
hectorhans <hector.romerosandon@gmail.com>
2020-11-11 17:04:51 -0500

    conexion a firebase y login
    conexion a firebase y login

--- Proyecto/app/src/main/java/com/upc/group/parkingapp/LoginActivity.java
+++ Proyecto/app/src/main/java/com/upc/group/parkingapp/LoginActivity.java
@@ -1,23 +1,44 @@
 package com.upc.group.parkingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Person;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.upc.group.parkingapp.modelos.Persona;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button btnEntrar;
     EditText txtUser, txtPassword;
+    FirebaseDatabase db;
+    DatabaseReference reference;
+    String tipoUsuario="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        inicializarFirebase();
         asignarReferencias();
+    }
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        db = FirebaseDatabase.getInstance();
+        reference = db.getReference();
     }
 
     private void asignarReferencias() {
@@ -28,14 +49,39 @@ public class LoginActivity extends AppCompatActivity {
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                validarUsuario();
+            }
+        });
+    }
+
+    private void validarUsuario() {
+        reference.child("Persona").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot item:snapshot.getChildren()) {
+                    Persona p = item.getValue(Persona.class);
+
+                    if (p.getUsuario().equals(txtUser.getText().toString())
+                            && p.getPassword().equals(txtPassword.getText().toString())) {
+                        tipoUsuario = p.getTipoUsuario();
+                        break;
+                    }
+                }
+
                 Intent intent;
-                if (txtUser.getText().toString().equals("admin") && txtPassword.getText().toString().equals("admin")) {
+                if (tipoUsuario.equals("A")) {
                     intent = new Intent(LoginActivity.this, MainMenuAdmin.class);
                     startActivity(intent);
-                } else if (txtUser.getText().toString().equals("usuario") && txtPassword.getText().toString().equals("usuario")) {
+                } else if (tipoUsuario.equals("C")) {
                     intent = new Intent(LoginActivity.this, MainMenuUsuario.class);
                     startActivity(intent);
                 }
+                tipoUsuario="";
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
             }
         });
     }
---
+++ Proyecto/app/src/main/java/com/upc/group/parkingapp/modelos/Persona.java
@@ -0,0 +1,89 @@
+package com.upc.group.parkingapp.modelos;
+
+public class Persona {
+
+    private String id;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private String celular;
+    private String dni;
+    private String usuario;
+    private String password;
+    private String tipoUsuario;
+
+    public Persona() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
