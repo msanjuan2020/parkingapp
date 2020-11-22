6f29f4f2680264696cefd87bc02c8ab2792791ee
angelalroca <angela.roca.marquez@gmail.com>
2020-11-22 02:59:23 -0500

    AROCA: ListarEmpresas_v0
    AROCA: ListarEmpresas_v0

---
+++ Proyecto/app/src/main/java/com/upc/group/parkingapp/ListarEmpresasFBActivity.java
@@ -0,0 +1,14 @@
+package com.upc.group.parkingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarEmpresasFBActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_empresas_f_b);
+    }
+}
